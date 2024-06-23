a0876ab67abbd894951c7c50a0a3fd28033a4f5c
vidit-p <112198253+vidit-p@users.noreply.github.com>
2024-06-23 13:38:32 -0400

    Changed title

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+document.title = 'Vidit Patel';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
